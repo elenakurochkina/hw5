de2a27d077458b807af64928b00298e815ed2c1f
Elena Kurochkina <e.kuro4kina@gmail.com>
2022-06-22 14:31:58 +0300

    task1 completed

---
+++ task1/program.cs
@@ -0,0 +1,35 @@
+﻿/*Задайте массив заполненный случайными положительными трёхзначными 
+числами. Напишите программу, которая покажет количество чётных чисел 
+в массиве.*/
+
+int[] InitArray(int len)
+{
+    int[] arrA = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        arrA[i] = new Random().Next(100, 999);
+        System.Console.Write($"{arrA[i]} | ");
+    }
+    System.Console.WriteLine();
+    return arrA;
+}
+
+int EvenNumbers(int[] arrA)
+{
+    int count = 0;
+    foreach(int element in arrA)
+    {
+        if (element %2 == 0)
+        {
+            count ++;
+
+        }
+    }
+    return count;
+}
+
+int[] arrA = InitArray(5);
+System.Console.WriteLine();
+int count = EvenNumbers(arrA);
+System.Console.WriteLine($"even's count is {count}");
+
