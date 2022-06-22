d11b1a32d3ddcf8660c06f98cf9eebd21a61ea47
Elena Kurochkina <e.kuro4kina@gmail.com>
2022-06-22 17:44:41 +0300

    task3 completed

---
+++ task3/program.cs
@@ -0,0 +1,34 @@
+﻿/*Задайте массив вещественных чисел. Найдите разницу между максимальным 
+и минимальным элементов массива.*/
+
+int[] InitArray(int len)
+{
+    int[] arrA = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        arrA[i] = new Random().Next(2, 9);
+        System.Console.Write($"{arrA[i]} | ");
+    }
+    System.Console.WriteLine();
+    return arrA;
+}
+
+int DiffMinMax(int[] arrA)
+{
+    int Diff = 0;
+    int Min = 9;
+    int Max = 2;
+
+    foreach (int element in arrA)
+    {
+        if (element > Max) Max = element;
+        if (element < Min) Min = element;
+        Diff = Max - Min;
+    }
+    return Diff;
+}
+
+int[] arrA = InitArray(3);
+System.Console.WriteLine();
+int Diff = DiffMinMax(arrA);
+System.Console.WriteLine($"difference between MAX & MIN = {Diff}");
